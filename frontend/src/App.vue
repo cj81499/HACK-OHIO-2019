1345b4197520af1a7fa306919a189501db810837
coltsaw <43895346+coltsaw@users.noreply.github.com>
2019-11-02 18:32:27 -0400

    add table component to app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,18 +2,31 @@
   <div id="app">
     <Header />
     <Navbar />
+    <div class="grid-container">
+      <div class="grid-items">
+        <Table />
+      </div>
+      <div class="grid-items">
+        <Table />
+      </div>
+      <div class="grid-items">
+        <Table />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Navbar from "./components/Navbar.vue";
+import Table from "./components/Table.vue";
 
 export default {
   name: "app",
   components: {
     Header,
-    Navbar
+    Navbar,
+    Table
   }
 };
 </script>
@@ -28,6 +41,20 @@ html,
 body {
   margin: 0;
   padding: 0;
+}
+
+.grid-container {
+  display: grid;
+  grid-column-gap: 10px;
+  grid-template-columns: auto auto auto;
+  padding: 10px;
+}
+.grid-items {
+  border: 0.5px solid #00aaff;
+  border-radius: 10px;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 2px 2px rgba(0, 0, 0, 0.12),
+    0 4px 4px rgba(0, 0, 0, 0.12), 0 8px 8px rgba(0, 0, 0, 0.12),
+    0 16px 16px rgba(0, 0, 0, 0.12);
 }
 
 #app {
