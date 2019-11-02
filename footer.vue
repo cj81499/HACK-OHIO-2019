b02cf4732ae2586a82e9c35408bdc58a9496707b
CarrieBT <trabue.18@buckeyemail.osu.edu>
2019-11-02 18:37:37 -0400

    sites footer

---
+++ footer.vue
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
+<body>
+
+<footer>
+<div><img src="https://www.engie.com/wp-content/themes/gdfsuez/assets/css/images/backgrounds/brand-colours_v2.jpg"><img></div>
+<div><div>
+	<ul>
+	<li>
+	<a href="https://www.engie.com/en/contact-2/">Need info? Then contact our teams</a></li>
+	<li><a href="https://www.engie.com/en/commitments/procurement/">Suppliers web portal</a></li>
+	<li><a href="https://www.engie.com/en/sitemap/">Sitemap</a></li>
+	</ul>
+	<ul>
+	<li>
+	<a href="https://www.engie.com/en/group-websites/">Group websites</a></li>
+	<li>
+	<a href="https://www.engie.com/en/accessibility/">Help and accessibility</a></li>
+	<li>
+	<a href="https://www.mediateur-engie.com/?lang=en">The ENGIE Group's Mediator</a>
+	</li>
+	</ul>
+	<ul>
+	<li>
+	<a href="https://www.engie.com/en/credits-website/">Credits</a>
+	<li>
+	<li>
+	<a href="https://www.engie.com/en/terms/">Terms of use</a>
+	</li>
+</ul>
+
+</div>
+<div>
+<div>
+	<a href="http://linkedin.com/company/engie" target="_blank"><img src = "images/linkdin-sprite.png"></a>
+	<a href="http://facebook.com/EngieFR" target="_blank"><img src = "images/facebook-sprite.png"></a>
+	<a href="http://twitter.com/engiegroup" target="_blank"><img src = "images/twitter-sprite.png"></a>
+	<a href="http://youtube.com/gdfsuez" target="_blank"><img src = "images/youtube-sprite.png"></a>
+	<a href="http://instagram.com/engie"><img src = "images/pinterest-sprite.png"></a>
+	<a href="https://www.engie.com/en/journalists/press-releases/" target="_blank"><img src = "images/rss-sprite.png"></a>
+</div>
+</div>
+</div>
+<div class = "tagline">
+<img alt="ENGIE" src="https://www.engie.com/wp-content/themes/gdfsuez/assets/images/logo-footer.jpg">
+</div>
+</footer>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+
+
+footer {
+ padding: 65px 40px;
+    width: 100%;
+
+font-family: Tahoma, Arial, Helvetica, FreeSans, sans-serif;
+ box-sizing: border-box;
+    display: block;
+    margin: 80px auto 0;
+    max-width: 1440px;
+    padding: 30px 0;
+    width: 98%;
+
+}
+
+ul {list-style: none;}
+
+
+a {
+  font-size: 1em;
+  transition: all 0.2s ease-in;
+  color: #999;
+  cursor: default;
+  text-decoration: none;
+
+}
+
+
+
+}
+.footer-right {
+    float: right;
+    width: 50%;
+}
+
+.footer-left {
+    float: left;
+    width: 50%;
+}
+</style>
+
+
+</body>
+</html>
