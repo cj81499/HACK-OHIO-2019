dda60b5453c281e00c798e55703cd1c89248538b
coltsaw <43895346+coltsaw@users.noreply.github.com>
2019-11-02 16:05:52 -0400

    create header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <img src="../assets/ENGIE-Logo.png" alt="ENGIE logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #00aaff;
+  padding: 10px;
+}
+img {
+  width: 15rem;
+}
+</style>
+
+
