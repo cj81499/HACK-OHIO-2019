10e8828fd12f7cd7c1c34db4e7720cc245c81b1d
coltsaw <43895346+coltsaw@users.noreply.github.com>
2019-11-02 22:58:31 -0400

    create $shadow variable

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -59,14 +59,12 @@ body {
 
 .grid-item {
   $shadow: rgba(0, 0, 0, 0.12);
-  box-shadow: 0 1px 1px $shadow, 0 2px 2px $shadow,
-    0 4px 4px $shadow, 0 8px 8px $shadow,
-    0 16px 16px $shadow;
+  box-shadow: 0 1px 1px $shadow, 0 2px 2px $shadow, 0 4px 4px $shadow,
+    0 8px 8px $shadow, 0 16px 16px $shadow;
   border: 1px solid #ccc;
   border-radius: $border-radius;
   padding: 1rem;
 }
-
 
 #app {
   display: flex;
