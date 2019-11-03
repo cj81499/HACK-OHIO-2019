fa92cdee9f51b18988b4cbd92ce648a064b2cdde
Devin <devkelly17@gmail.com>
2019-11-03 00:21:23 -0400

    Added dashboard component and router functionality

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,7 @@
   <div id="app">
     <Header />
     <Navbar />
-    <div class="grid-container">
-      <div class="grid-items">
-        <Table />
-      </div>
-      <div class="grid-items">
-        <MapPreview />
-      </div>
-      <div class="grid-items"></div>
-    </div>
+    <router-view/>
     <Footer />
   </div>
 </template>
@@ -18,8 +10,6 @@
 <script>
 import Header from "./components/Header.vue";
 import Navbar from "./components/Navbar.vue";
-import MapPreview from "./components/MapPreview.vue";
-import Table from "./components/Table.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
@@ -27,9 +17,7 @@ export default {
   components: {
     Header,
     Navbar,
-    MapPreview,
-    Table,
-    Footer
+    Footer,
   }
 };
 </script>
@@ -48,25 +36,6 @@ body {
   padding: 0;
   height: 100%;
 }
-
-.grid-container {
-  display: grid;
-  grid-column-gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
-  padding: 10px;
-  flex-grow: 1;
-}
-
-.grid-items {
-  $shadow: rgba(0, 0, 0, 0.12);
-  box-shadow: 0 1px 1px $shadow, 0 2px 2px $shadow,
-    0 4px 4px $shadow, 0 8px 8px $shadow,
-    0 16px 16px $shadow;
-  border: 1px solid #ccc;
-  border-radius: $border-radius;
-  padding: 1rem;
-}
-
 
 #app {
   display: flex;
---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="grid-container">
+      <div class="grid-items">
+        <Table />
+      </div>
+      <div class="grid-items">
+        <MapPreview />
+      </div>
+      <div class="grid-items"></div>
+    </div>
+</template>
+
+<script>
+import MapPreview from "./MapPreview.vue";
+import Table from "./Table.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    MapPreview,
+    Table,
+  }
+};
+</script>
+
+<style lang="scss">
+    @import "../styles/constants.scss";
+    
+    .grid-container {
+    display: grid;
+    grid-column-gap: 10px;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 10px;
+    flex-grow: 1;
+    }
+
+    .grid-items {
+    $shadow: rgba(0, 0, 0, 0.12);
+    box-shadow: 0 1px 1px $shadow, 0 2px 2px $shadow,
+        0 4px 4px $shadow, 0 8px 8px $shadow,
+        0 16px 16px $shadow;
+    border: 1px solid #ccc;
+    border-radius: $border-radius;
+    padding: 1rem;
+    }
+</style>
